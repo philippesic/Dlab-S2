362de8581fd61defc2a6c9251c93838c5af8f8c5
Pippo Pesic <philippesic06@gmail.com>
2023-01-25 19:21:42 -0800

    Added saounds effects for crash and Level 1 enemy sprite

--- Scripts/Crash.gd
+++ Scripts/Crash.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 func _ready():
+	$Sound1.play()
 	$Crash1.visible = false
 	$Crash2.visible = true
-	yield(get_tree().create_timer(4), "timeout")
+	yield(get_tree().create_timer(4.5), "timeout")
+	$Sound1.stop()
+	$Sound2.play()
 	$Crash1.visible = true
 	$Crash2.visible = false
+	yield(get_tree().create_timer(1.7), "timeout")
+	get_tree().change_scene("res://Scenes/Title.tscn")
--- Scripts/Level1.gd
+++ Scripts/Level1.gd
@@ -21,7 +21,7 @@ func _on_enemy_died():
 	emit_signal("updateAmmo")
 
 func _ready():
-	yield(get_tree().create_timer(30), "timeout")
+	yield(get_tree().create_timer(3), "timeout")
 	get_tree().change_scene("res://Scenes/Level2.tscn")
 	queue_free()
 

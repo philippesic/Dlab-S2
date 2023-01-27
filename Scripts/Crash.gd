60ba10c02053ddd05fe5d2f084e5d4d1db99c61d
Pippo Pesic <philippesic06@gmail.com>
2023-01-26 21:13:51 -0800

    Fixed crash screen not properly displaying text

--- Scripts/Crash.gd
+++ Scripts/Crash.gd
@@ -2,14 +2,9 @@ extends Node2D
 
 func _ready():
 	$Sound1.play()
-	$Recomputingtext.visible = false
-	$Crash1.visible = false
-	$Crash2.visible = true
 	yield(get_tree().create_timer(4.5), "timeout")
 	$Sound1.stop()
 	$Sound2.play()
-	$Recomputingtext.visible = true
-	$Crash1.visible = true
 	$Crash2.visible = false
 	yield(get_tree().create_timer(1.7), "timeout")
 	get_tree().change_scene("res://Scenes/Title.tscn")
